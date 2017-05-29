fc83e66a4f1e281b3ad3238a7ed34bc29a9a6540
Mariano D'Agostino <mariano@tictapps.com>
2017-05-29 19:44:37 -0300

    Import project

---
+++ Prueba/Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Mariano D'Agostino on 5/29/17.
+//  Copyright © 2017 Tictapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
