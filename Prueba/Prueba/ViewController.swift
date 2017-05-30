8e98741725649f21ae8bdf5bc4b14feab6ac8680
Mariano D'Agostino <mariano@tictapps.com>
2017-05-29 21:01:54 -0300

    Verde

--- Prueba/Prueba/ViewController.swift
+++ Prueba/Prueba/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.green
     }
 
     override func didReceiveMemoryWarning() {
